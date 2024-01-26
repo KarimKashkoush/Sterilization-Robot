5608fbfa23412b17b285178ff74a73b86ec8beb3
KarimKashkoush <karimkashkoush5@gmail.com>
2024-01-26 17:43:39 +0200

    add

--- assets/js/main.js
+++ assets/js/main.js
@@ -10,34 +10,41 @@ function signUp() {
     form.style.right = "20px"
 }
 
-
 function dataTimer() {
     let arrData = [];
-
+    
+    // Input Data From user
     let nameFormTimer = document.getElementById("nameFormTimer")
     let idFormTimer = document.getElementById("idFormTimer")
     let timeFormTimer = document.getElementById("timeFormTimer")
     let locationFormTimer = document.getElementById("locationFormTimer")
     
+    // object Data
     let newTimer = {
         user: nameFormTimer.value,
         userId: idFormTimer.value,
         timeTimer: timeFormTimer.value,
         place: locationFormTimer.value
     }
-    
+
+    // push Data in arr
+    arrData.push(newTimer)
+
+    // Data value to Show
     let nameShow = document.getElementById("nameShow")
     let idShow = document.getElementById("idShow")
     let timeShow = document.getElementById("timeShow")
     let placeShow = document.getElementById("placeShow")
     let timer = document.getElementById("timer")
 
+    // Convert minutes to a timer
+    let hours = Math.floor(arrData[arrData.length - 1].timeTimer / 60)
+    let min = Math.floor(arrData[arrData.length - 1].timeTimer % 60)
+    let sec = (arrData[arrData.length - 1].timeTimer * 60) % 60
 
-    let hours = Math.floor(timeFormTimer.value / 60)
-    let min = Math.floor(timeFormTimer.value % 60)
-    let sec = (timeFormTimer.value * 60) % 60
+    var time = setInterval (counter, 1000)
 
-    setInterval(() => {
+    function counter() {
         sec--
         if (sec == -1) {
             sec = 59;
@@ -47,25 +54,43 @@ function dataTimer() {
             min = 59
             hours--
         }
-    
-        
-        
-    
-        if (+(timeFormTimer.value) == 0 || timeFormTimer.value == "") {
+
+        if (+(arrData[arrData.length - 1].timeTimer) == 0 || arrData[arrData.length - 1].timeTimer == "") {
             timer.innerHTML = "لا يوجد معلومات"
         } else {
             timer.innerHTML = `${sec} : ${min} : ${hours}`
         }
-    }, 1000);
+        
+        if (hours == 0 && min == 0 && sec == 0) {
+            clearInterval(time)
+            timer.innerHTML = "انتهي وقت التعقيم"
 
+            setTimeout(()=>{
+                location.reload()
+            }, 3000)
+        }
+    }
+
+    // Show All Data
     nameShow.innerHTML = newTimer.user
     idShow.innerHTML = newTimer.userId
     timeShow.innerHTML = `${newTimer.timeTimer} دقيقة`
     placeShow.innerHTML = newTimer.place
     timer.innerHTML = newTimer.place
+
+    let boxs = document.querySelectorAll(".container .box")
+
+    boxs.forEach((e)=> {
+        if (e.querySelector("h2").innerHTML === placeShow.innerHTML) {
+            console.log(e)
+        }
+    })
 }
 
+
+
 function closeTimer() {
+    location.reload()
     let showTime = document.getElementById("showTime")
     showTime.style.top ="-100%"
 }
@@ -74,3 +99,4 @@ function openTimer() {
     let showTime = document.getElementById("showTime")
     showTime.style.top ="50%"
 }
+
