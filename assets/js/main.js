9cb67a99eb70dc73196de3399739c96492d8b88d
KarimKashkoush <karimkashkoush5@gmail.com>
2024-01-26 14:45:13 +0200

    add

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,16 +1,19 @@
-
 function signIn() {
     let form = document.getElementById("form")
-    form.style.right = "-350px"
+    form.style.right = ""
+    form.style.left = "20px"
 }
 
 function signUp() {
-    form.style.right = "10px"
+    let form = document.getElementById("form")
+    form.style.left = ""
+    form.style.right = "20px"
 }
 
+
 function dataTimer() {
-    let arrData = []
-    
+    let arrData = [];
+
     let nameFormTimer = document.getElementById("nameFormTimer")
     let idFormTimer = document.getElementById("idFormTimer")
     let timeFormTimer = document.getElementById("timeFormTimer")
@@ -22,19 +25,44 @@ function dataTimer() {
         timeTimer: timeFormTimer.value,
         place: locationFormTimer.value
     }
-
-    arrData.push(newTimer)
     
     let nameShow = document.getElementById("nameShow")
     let idShow = document.getElementById("idShow")
     let timeShow = document.getElementById("timeShow")
     let placeShow = document.getElementById("placeShow")
+    let timer = document.getElementById("timer")
+
+
+    let hours = Math.floor(timeFormTimer.value / 60)
+    let min = Math.floor(timeFormTimer.value % 60)
+    let sec = (timeFormTimer.value * 60) % 60
+
+    setInterval(() => {
+        sec--
+        if (sec == -1) {
+            sec = 59;
+            min--
+        }
+        if (min == -1) {
+            min = 59
+            hours--
+        }
+    
+        
+        
+    
+        if (+(timeFormTimer.value) == 0 || timeFormTimer.value == "") {
+            timer.innerHTML = "لا يوجد معلومات"
+        } else {
+            timer.innerHTML = `${sec} : ${min} : ${hours}`
+        }
+    }, 1000);
 
     nameShow.innerHTML = newTimer.user
     idShow.innerHTML = newTimer.userId
-    timeShow.innerHTML = newTimer.timeTimer
+    timeShow.innerHTML = `${newTimer.timeTimer} دقيقة`
     placeShow.innerHTML = newTimer.place
-
+    timer.innerHTML = newTimer.place
 }
 
 function closeTimer() {
@@ -46,5 +74,3 @@ function openTimer() {
     let showTime = document.getElementById("showTime")
     showTime.style.top ="50%"
 }
-
-
